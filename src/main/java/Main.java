6c4b45ed934a469ea162028c81359edf92007a21
Vvv27v <vithues@hotmail.fr>
2026-05-29 10:12:20 +0200

    Skip login, launch interface directly for testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new LoginFrame());
+        javax.swing.SwingUtilities.invokeLater(() -> new InterfaceGraphique(1));
     }
 }
